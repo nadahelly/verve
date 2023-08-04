4caa5e9fe90f21de3a504476e82c7ba520690f40
nadahelly <nadahelly@naver.com>
2023-08-04 18:06:29 +0900

    goods-list,event,event_detail

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -7,5 +7,14 @@ var swiper = new Swiper(".slide-g", {
   },
 });
 
-var swiper = new Swiper(".sale-slide", {});
-
+var swiper = new Swiper(".sale-slide", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
