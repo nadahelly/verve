c4acdba9bd866a05095cd8c5dc73112577274f29
nadahelly <nadahelly@naver.com>
2023-08-08 18:15:17 +0900

    event_slide

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -18,3 +18,22 @@ var swiper = new Swiper(".sale-slide", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+var slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName("slide_item");
+       
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1
+        }
+        slides[slideIndex - 1].style.display = "block";
+    
+        setTimeout(showSlides, 3500); // 2초마다 이미지가 체인지됩니다
+    }
